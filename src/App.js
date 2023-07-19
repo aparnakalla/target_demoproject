3dfcd354c4584b1aaf571a117f24890be1fd8493
aparnakalla <aparnakalla11@gmail.com>
2023-07-19 19:46:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState(""); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="col-md-12">
+        <button class="btn btn-danger pull-right">Log Out</button>
+      </div>
+      <div class="col-md-12">
+        <button class="btn btn-primary pull-left">View all students</button>
+      </div>
+      <div class="col-md-12 text-center">
+        <button class="btn btn-primary pull-left">Add Student </button>
+     </div>
+      <h1> Student Search </h1>
+      <label> Search Student</label>
+      <input type="text" onChange={e => setQuery(e.target.value)} />
+      <div class="col-md-12 text-center">
+        <button class="btn btn-primary">Search </button>
+     </div>
+     
     </div>
   );
 }
